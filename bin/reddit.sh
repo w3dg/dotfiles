3877e7440fecb4b45ba251864104607a16d2164c
w3dg <51162936+w3dg@users.noreply.github.com>
2024-04-13 18:26:28 +0530

    open in browser

--- bin/reddit.sh
+++ bin/reddit.sh
@@ -10,5 +10,4 @@ baseUrl=https://old.reddit.com
 json=$(curl -s  $baseUrl/r/$subname/new.json)
 
 selectedPost=`echo $json | jq -r '.data.children | .[] | "\(.data.title)\t\(.data.permalink)"' | fzf --delimiter='\t' --with-nth=1 |cut -f2`
-echo $baseUrl$selectedPost | clip
-echo "Post link copied to clipboard"
+explorer $baseUrl$selectedPost 
